503a369e7b9b331c34d7b4b9b92804945e0a1a61
connorhw <connorwong93@gmail.com>
2020-04-15 23:01:10 -0700

    after this commit, first attempt at deploying to prod.

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -24,7 +25,6 @@ app.use(function validateBearerToken(req, res, next) {
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  //debugger
   //move to the next middleware
   next()
 })
@@ -36,13 +36,10 @@ app.get('/types', handleGetTypes)
 
 /*  get pokemon from query string param */
 function handleGetPokemon(req, res) {
-  //console.log(req.query)
-  //res.send('Hello, Pokemon!')
 }
 
 app.get('/pokemon', function handleGetPokemon(req, res){
   let response = POKEDEX.pokemon;
-  //console.log(response);
   if (req.query.name) {
     response = response.filter(pokemon =>
       // case insensitive searching
@@ -60,7 +57,17 @@ app.get('/pokemon', function handleGetPokemon(req, res){
   res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
